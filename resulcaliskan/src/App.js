b4d90c6bebf770da07cefbfd5fcd497a5bbfb158
Resul Caliskan <resulcaliskansau@gmail.com>
2025-06-09 15:32:19 +0300

    feat: new style css

--- resulcaliskan/src/App.js
+++ resulcaliskan/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Head from './components/Head';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+import Preloader from './components/Preloader';
+import './style.css';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <Head />
+      {isLoading ? <Preloader /> : (
+        <>
+          <Header />
+          <Main />
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
---
+++ resulcaliskan/src/components/Head.js
@@ -0,0 +1,44 @@
+// components/Head.js
+import React from 'react';
+
+const Head = () => {
+  return (
+    <>
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="robots" content="index, follow" />
+      <meta name="theme-color" content="#000" />
+      <meta
+        name="description"
+        content="Resul Çalışkan | Portfolio, Visit and know about me & my projects/designs. I value simple content structure, clean design patterns, and thoughtful interactions."
+      />
+      <meta
+        name="keywords"
+        content="Resul Çalışkan,Resul Çalışkan portfolio,Resul Çalışkan Portfolio,vinodjangid,Resul Çalışkan site,Resul Çalışkan Site,Resul Çalışkan me,Resul Çalışkan web,Resul Çalışkan github,vinodjangid site,vinodjangid uiverse,uiverse,Resul Çalışkan uiverse,Resul Çalışkan frontend developer,frontend developer,Resul Çalışkan jaipur,vinod jaipur"
+      />
+      <meta name="author" content="Resul Çalışkan" />
+
+      <meta property="og:title" content="Resul Çalışkan | Portfolio" />
+      <meta property="og:description"
+        content="Hi! My name is Vinod. I'm a web developer and digital visual artist. Visit and know about me & my projects/designs. I value simple content structure, clean design patterns, and thoughtful interactions."
+      />
+      <meta property="og:url" content="https://vinodjangid.site" />
+      <meta property="og:site_name" content="Resul Çalışkan Portfolio" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@Vinod_Jangid07" />
+      <meta name="twitter:title" content="Resul Çalışkan | Portfolio" />
+      <meta name="twitter:description"
+        content="Hi! My name is Vinod. I'm a web developer and digital visual artist. Visit and know about me & my projects/designs. I value simple content structure, clean design patterns, and thoughtful interactions."
+      />
+      <title>Resul Çalışkan | Portfolio</title>
+      <link rel="icon" type="image/x-icon" href="src/png/main-favicon.png" />
+      <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+      <link rel="stylesheet" href="style.css" />
+      <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Passions+Conflict&family=Orbitron&family=Fira+Code&family=Six+Caps" />
+    </>
+  );
+};
+
+export default Head;
