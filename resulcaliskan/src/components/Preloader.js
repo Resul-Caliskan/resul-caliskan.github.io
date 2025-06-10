82b549e12bee9fe1e1e17862c75cbfe227512d52
Resul Caliskan <resulcaliskansau@gmail.com>
2025-06-10 11:37:28 +0300

    feat: loader css changed

--- resulcaliskan/src/components/Preloader.js
+++ resulcaliskan/src/components/Preloader.js
@@ -1,13 +1,30 @@
 // components/Preloader.js
-import React from 'react';
-
+import React from "react";
+import "./hover.css";
 const Preloader = () => {
   return (
     <>
-      <audio loop id="audioPlayer" style={{ display: 'none' }}>
+      <audio loop id="audioPlayer" style={{ display: "none" }}>
         <source src="src/mp3/preloader.mp3" type="audio/mp3" />
       </audio>
-      <div id="preloader"></div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh", // sayfanın tamamını kaplaması için
+          flexDirection: "column",
+        }}
+      >
+        <div class="wrapper">
+          <div class="circle"></div>
+          <div class="circle"></div>
+          <div class="circle"></div>
+          <div class="shadow"></div>
+          <div class="shadow"></div>
+          <div class="shadow"></div>
+        </div>
+      </div>
       <noscript>Allow Javascript</noscript>
     </>
   );
